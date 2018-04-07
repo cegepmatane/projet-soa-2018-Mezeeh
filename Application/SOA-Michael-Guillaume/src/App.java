045a15216704423ffcd3125db77e2e0d183aab0e
Mezeeh <1643216@cgmatane.qc.ca>
2018-04-06 21:47:36 -0400

    Coquille Application

---
+++ Application/SOA-Michael-Guillaume/src/App.java
@@ -0,0 +1,8 @@
+import vue.VaisseauVue;
+
+public class App {
+    public static void main(String[] args) {
+        VaisseauVue vaisseauVue = new VaisseauVue();
+        vaisseauVue.launch(VaisseauVue.class, args);
+    }
+}
