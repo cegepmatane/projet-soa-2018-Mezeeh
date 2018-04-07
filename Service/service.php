49ca94d657a64823c81a5f76bcdebc147bdde29b
gui45 <guillaume.d.marceau@gmail.com>
2018-04-06 21:27:34 -0400

    xml service

--- Service/service.php
+++ Service/service.php
@@ -1,4 +1,40 @@
 <?php
-  include_once "ServiceDao.php"
-  $ServiceDao = new ServiceDao();
+  include "ServiceDao.php";
+  $serviceDao = new ServiceDao();
+  $listeVaisseaux = $serviceDao->lireVaisseau();
+  $listeVoyages = $serviceDao->lireVoyage();
+  //print_r($listeVaisseaux);
+
+  header("Content-type: text/xml");
+  echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
+<servicevoyage>
+  <vaisseaux>
+      <?php
+          foreach($listeVaisseaux as $vaisseau){?>
+              <vaisseau>
+                  <id><?=$vaisseau['id']?></id>
+                  <nom><?=$vaisseau['nom']?></nom>
+                  <description><?=$vaisseau['description']?></description>
+                  <capaciter><?=$vaisseau['capaciter']?></capaciter>
+                  <porter><?=$vaisseau['porter']?></porter>
+              </vaisseau>
+              <?php
+          }
+      ?>
+  </vaisseaux>
+  <voyages>
+      <?php
+          foreach($listeVoyages as $voyages){?>
+              <vaisseau>
+                  <id><?=$voyages['id']?></id>
+                  <nom><?=$voyages['nom']?></nom>
+                  <description><?=$voyages['destination']?></description>
+                  <capaciter><?=$voyages['description']?></capaciter>
+                  <porter><?=$voyages['distance']?></porter>
+              </vaisseau>
+              <?php
+          }
+      ?>
+  </voyages>
+</servicevoyage>
