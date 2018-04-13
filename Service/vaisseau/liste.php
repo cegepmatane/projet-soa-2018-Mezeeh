c21f1ab7c23698f12034dca124a2607717558457
gui45 <guillaume.d.marceau@gmail.com>
2018-04-12 20:05:04 -0400

    ajouter liste vaisseau

---
+++ Service/vaisseau/liste.php
@@ -0,0 +1,19 @@
+<?php
+  include "ServiceDao.php";
+  $serviceDao = new ServiceDao();
+  $listeVaisseaux = $serviceDao->lireVaisseaux();
+  //print_r($listeVaisseaux);
+  header("Content-type: text/xml");
+  echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<vaisseaux>
+  <?php foreach($listeVaisseaux as $vaisseau){?>
+      <vaisseau>
+          <id><?=$vaisseau['id']?></id>
+          <nom><?=utf8_encode($vaisseau['nom'])?></nom>
+          <description><?=utf8_encode($vaisseau['description'])?></description>
+          <capaciter><?=utf8_encode($vaisseau['capaciter'])?></capaciter>
+          <porter><?=$vaisseau['porter']?></porter>
+      </vaisseau>
+    <?php } ?>
+  </vaisseaux>
