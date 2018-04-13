5d81cb41a2532148943bdc5ee4cfa32760d7f558
gui45 <guillaume.d.marceau@gmail.com>
2018-04-12 20:43:46 -0400

    xml pour les vaisseau individuel

--- Service/voyage/voyage.php
+++ Service/voyage/voyage.php
@@ -2,5 +2,15 @@
   include_once "../serviceDao.php";
   $dao = new ServiceDao();
   $voyage = $dao->lireVoyage($_GET['id']);
-  var_dump($voyage);
+  //var_dump($voyage);
+  header("Content-type: text/xml");
+  echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
+<voyage>
+    <id><?=$voyage['id']?></id>
+    <nom><?=utf8_encode($voyage['nom'])?></nom>
+    <destination><?=utf8_encode($voyage['destination'])?></destination>
+    <description><?=utf8_encode($voyage['description'])?></description>
+    <distance><?=$voyage['distance']?></distance>
+    <idvaisseau><?=$voyage['idvaisseau']?></idvaisseau>
+</voyage>
