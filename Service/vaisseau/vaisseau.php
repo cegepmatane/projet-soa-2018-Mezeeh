4ac4370cd2fdaa727ac52181c7636f7c48596cf3
gui45 <guillaume.d.marceau@gmail.com>
2018-04-12 20:37:07 -0400

    completer la page xml du vaisseau individuel

--- Service/vaisseau/vaisseau.php
+++ Service/vaisseau/vaisseau.php
@@ -2,5 +2,14 @@
   include_once "../serviceDao.php";
   $dao = new ServiceDao();
   $vaisseau = $dao->lireVaisseau($_GET['id']);
-  var_dump($vaisseau);
+  //var_dump($vaisseau);
+  header("Content-type: text/xml");
+  echo '<?xml version="1.0" encoding="UTF-8"?>';
  ?>
+ <vaisseau>
+     <id><?=$vaisseau['id']?></id>
+     <nom><?=utf8_encode($vaisseau['nom'])?></nom>
+     <description><?=utf8_encode($vaisseau['description'])?></description>
+     <capaciter><?=utf8_encode($vaisseau['capaciter'])?></capaciter>
+     <porter><?=$vaisseau['porter']?></porter>
+ </vaisseau>
