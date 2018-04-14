185ea3d9b5818024f60c343bf627f420f0dcbf47
gui45 <guillaume.d.marceau@gmail.com>
2018-04-13 22:06:22 -0400

    terminer xml voyage/voyagesVaisseau.php

--- Service/voyage/voyagesVaisseau.php
+++ Service/voyage/voyagesVaisseau.php
@@ -2,5 +2,19 @@
   include_once "../ServiceDao.php";
   $dao = new ServiceDao();
   $voyages = $dao->lireVoyagesVaisseau($_GET['id']);
-  var_dump($voyages);
- ?>
+  //var_dump($voyages);
+  header("Content-type: text/xml");
+  echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<voyages>
+  <?php foreach($voyages as $voyage){?>
+      <voyage>
+          <id><?=$voyage['id']?></id>
+          <nom><?=utf8_encode($voyage['nom'])?></nom>
+          <destination><?=utf8_encode($voyage['destination'])?></destination>
+          <description><?=utf8_encode($voyage['description'])?></description>
+          <distance><?=$voyage['distance']?></distance>
+          <idvaisseau><?=$voyage['idvaisseau']?></idvaisseau>
+      </voyage>
+    <?php } ?>
+  </voyages>
