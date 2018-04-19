032943bd64b77f7c6af12adb76d4bc534b8431d6
gui45 <guillaume.d.marceau@gmail.com>
2018-04-19 17:01:30 -0400

    ajouter la fonction ajouter du dao placer le sql dans leur propres variables

--- Service/ServiceDao.php
+++ Service/ServiceDao.php
@@ -5,7 +5,8 @@
     function lireVaisseaux()
     {
       global $basededonnees;
-      $requete = $basededonnees->prepare("SELECT * FROM vaisseau");
+      $SQL_LIRE_VAISSEAUS = "SELECT * FROM vaisseau";
+      $requete = $basededonnees->prepare($SQL_LIRE_VAISSEAUS);
       $requete->execute();
       $vaisseaux = $requete->fetchAll();
 
@@ -13,8 +14,9 @@
     }
     function lireVoyages()
     {
+      $SQL_LIRE_VOYAGES = "SELECT * FROM voyage";
       global $basededonnees;
-      $requete = $basededonnees->prepare("SELECT * FROM voyage");
+      $requete = $basededonnees->prepare($SQL_LIRE_VOYAGES);
       $requete->execute();
       $voyages = $requete->fetchAll();
 
@@ -23,7 +25,8 @@
     function lireVoyage($id)
     {
       global $basededonnees;
-      $requete = $basededonnees->prepare("SELECT * FROM voyage WHERE id = :id");
+      $SQL_LIRE_VOYAGE = "SELECT * FROM voyage WHERE id = :id";
+      $requete = $basededonnees->prepare($SQL_LIRE_VOYAGE);
       $requete->bindParam(':id', $id, PDO::PARAM_INT);
       $requete->execute();
       $voyage = $requete->fetch();
@@ -33,7 +36,8 @@
     function lireVoyagesVaisseau($id)
     {
       global $basededonnees;
-      $requete = $basededonnees->prepare("SELECT * FROM voyage WHERE idvaisseau = :id");
+      $SQL_LIRE_VOYAGES_VAISSEAU = "SELECT * FROM voyage WHERE idvaisseau = :id";
+      $requete = $basededonnees->prepare($SQL_LIRE_VOYAGES_VAISSEAU);
       $requete->bindParam(':id', $id, PDO::PARAM_INT);
       $requete->execute();
       $voyages = $requete->fetchAll();
@@ -43,12 +47,26 @@
     function lireVaisseau($id)
     {
       global $basededonnees;
-      $requete = $basededonnees->prepare("SELECT * FROM vaisseau WHERE id = :id");
+      $SQL_LIRE_VAISSEAU = "SELECT * FROM vaisseau WHERE id = :id";
+      $requete = $basededonnees->prepare($SQL_LIRE_VAISSEAU);
       $requete->bindParam(':id', $id, PDO::PARAM_INT);
       $requete->execute();
       $vaisseau = $requete->fetch();
 
       return $vaisseau;
+    }
+    function ajouterVoyages($voyage)
+    {
+      global $basededonnees;
+			$SQL_AJOUTER_VOYAGE = "INSERT INTO voyage(id, nom, destination, description, distance, idvaisseau) VALUES(:id, :nom, :destination, :description, :distance, :idvaisseau)";
+			$requeteAjouter = $basededonnees->prepare($SQL_AJOUTER_VOYAGE);
+			$requeteAjouter->bindParam(':id', $pensee['id'], PDO::PARAM_STR);
+			$requeteAjouter->bindParam(':nom', $pensee['nom'], PDO::PARAM_STR);
+			$requeteAjouter->bindParam(':description', $pensee['destination'], PDO::PARAM_STR);
+			$requeteAjouter->bindParam(':description',$pensee['description'], PDO::PARAM_STR);
+      $requeteAjouter->bindParam(':distance', $pensee['distance'], PDO::PARAM_STR);
+      $requeteAjouter->bindParam(':idvaisseau',$pensee['idvaisseau'], PDO::PARAM_STR);
+			$requeteAjouter->execute();
     }
   }
 ?>
