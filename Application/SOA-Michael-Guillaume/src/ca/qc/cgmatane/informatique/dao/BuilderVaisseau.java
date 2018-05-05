0de6fa83aa6f81788ceab7a9cd1dd415b9881dad
gui45 <guillaume.d.marceau@gmail.com>
2018-05-04 21:59:29 -0400

    ajouter constantes

--- Application/SOA-Michael-Guillaume/src/ca/qc/cgmatane/informatique/dao/BuilderVaisseau.java
+++ Application/SOA-Michael-Guillaume/src/ca/qc/cgmatane/informatique/dao/BuilderVaisseau.java
@@ -7,35 +7,11 @@ import ca.qc.cgmatane.informatique.modele.Voyage;
 
 public class BuilderVaisseau {
 	 protected int id, capaciter, porter;
-	protected String nom, description;
+	 protected String nom, description;
 	 protected List<Voyage> listeVoyages;
-	 
-	 public BuilderVaisseau setId(int id) {
-			this.id = id;
-			return this;
-	}
-	public BuilderVaisseau setCapaciter(int capaciter) {
-		this.capaciter = capaciter;
+	 protected String typeParseur;
+	 public BuilderVaisseau SetParseur(String type){
+		this.typeParseur = type;
 		return this;
-	}
-	public BuilderVaisseau setPorter(int porter) {
-		this.porter = porter;
-		return this;
-	}
-	public BuilderVaisseau setNom(String nom) {
-		this.nom = nom;
-		return this;
-	}
-	public BuilderVaisseau setDescription(String description) {
-		this.description = description;
-		return this;
-	}
-	public BuilderVaisseau setListeVoyages(List<Voyage> listeVoyages) {
-		this.listeVoyages = listeVoyages;
-		return this;
-	}
-	public Vaisseau buildVaisseau()
-	{
-		return new Vaisseau(id, nom, description, capaciter, porter);
-	}
+	 }
 }
---
+++ Application/SOA-Michael-Guillaume/src/ca/qc/cgmatane/informatique/dao/TypeParseur.java
@@ -0,0 +1,6 @@
+package ca.qc.cgmatane.informatique.dao;
+
+public interface TypeParseur {
+	public static final String XML = "XML";
+	public static final String JSON = "JSON";
+}
